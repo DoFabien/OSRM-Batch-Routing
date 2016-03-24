545a0db4b7783a44642cc0657e1a7165aff95a2f
DoFabien <fabien.delolmo@gmail.com>
2016-03-24 18:08:27 +0100

    url localhost

--- php/config.php
+++ php/config.php
@@ -2,7 +2,6 @@
 session_start();
 $_path = '/tmp/';
 $path_file = $_path .'osrmiti_'.session_id().'.geojson';
-//$osrm_url = 'http://localhost:5001';
-$osrm_url = 'http://dogeo.fr:5001';
+$osrm_url = 'http://localhost:5001';
 
 ?>
